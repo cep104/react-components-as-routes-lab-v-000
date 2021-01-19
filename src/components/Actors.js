80629a80ca2623cc0d40e2528438cc760d420dfa
cep104 <candicelizabethpeters@gmail.com>
2021-01-19 00:25:37 -0600

    notes

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {/* //map over actors data with index for key */}
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h3>Name: {actor.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {/* //map over the actors movies with index for keys */}
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>{movie.title}</h3>
+          <h4>{movie.time}</h4>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+          <h4>{movie.metascore}</h4>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default Movies;
+
+// title: 'Doctor Strange',
+//     time: 115,
+//     genres: ['Action', 'Adventure', 'Fantasy'],
+//     metascore: 74
